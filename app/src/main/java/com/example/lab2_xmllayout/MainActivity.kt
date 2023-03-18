e9de8495d1f458a5059cb6b70a3c32fc7c9afe3d
Samyam2002 <ssamyam20@tbc.edu.np>
2023-03-18 15:09:06 +0545

    add comments

--- app/src/main/java/com/example/lab2_xmllayout/MainActivity.kt
+++ app/src/main/java/com/example/lab2_xmllayout/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.lab2_xmllayout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Since we are building the layout for a basic tip calculator app, we have made changes only
+ * in the activity_main.xml file
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
